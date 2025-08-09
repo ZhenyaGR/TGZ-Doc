52b6af024077b7242fdd8ed926fd90789b74849d
Zhenya <em167116@gmail.com>
2025-08-09 13:05:08 +0300

    commit

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,5 +2,9 @@ import DefaultTheme from 'vitepress/theme'
 import './style.css' // Подключаем кастомные стили
 
 export default {
+    // Добавляем секцию head для иконки
+    head: [
+        ['link', { rel: 'icon', href: '/TGZ3.ico' }] // Путь к вашей иконке
+    ],
     ...DefaultTheme
 }
